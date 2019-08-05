c17fcbb47905ba73da5e9819685efcec34c60dcb
Yuki <fabbanana1980@gmail.com>
2019-08-05 11:58:08 +0000

    Finish Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,49 @@
 class BooksController < ApplicationController
+  def top
+  end
+
   def index
+    @books = Book.all.order(created_at: :desc)
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
   end
 
-  def edit
+  def create
+    book = Book.new(book_params)
+    if book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book)
+    end
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = 'Book was successfully update.'
+      redirect_to book_path(book)
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
